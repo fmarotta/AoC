856bd87e769864c2735f1ea4a5886f4a23e991ec
Federico Marotta <fmarotta@protonmail.com>
2023-12-05 11:14:17 +0100

    2023 day 5, code that I'm not so ashamed of

--- 2023/day5/run.jl
+++ 2023/day5/run.jl
@@ -1,108 +1,80 @@
-# function read_puzzle(file)
-#     seeds = []
-#     open(file, "r") do io
-#         seeds = parse.(Int, split(split(readline(io), ": ")[2]))
-#         maps = split(read(io, String), "\n\n")
-#         for map in maps
-#             mapped_seeds = copy(seeds)
-#             for (i, seed) in enumerate(seeds)
-#                 for line in split(map, "\n")
-#                     if line == ""
-#                         continue
-#                     elseif occursin(':', line)
-#                         name = split(line)[1]
-#                         println("\n", name)
-#                     else
-#                         # try to map each seed to this category
-#                         dest_start, source_start, len = parse.(Int, split(line))
-#                         if source_start <= seed <= source_start + len
-#                             mapped_seeds[i] = dest_start + seed - source_start
-#                             println("mapping $seed to $(mapped_seeds[i])")
-#                             break
-#                         end
-#                     end
-#                 end
-#             end
-#             println("mapping $seeds to $mapped_seeds")
-#             seeds = mapped_seeds
-#         end
-#         println("mapped  $seeds")
-#     end
-#     println( minimum(seeds))
-# end
+using DataStructures: OrderedDict
 
 function read_puzzle(file)
     open(file, "r") do io
-        pre_seeds = parse.(Int, split(split(readline(io), ": ")[2]))
-        seeds = []
-        for (i, pre_seed) in enumerate(pre_seeds)
-            if i & 1 == 0
-                continue
-            end
-            push!(seeds, pre_seed:pre_seed+pre_seeds[i+1]-1)
-        end
-        # println(seeds)
-        maps = split(read(io, String), "\n\n")
-        for map in maps
-            mapped_seeds = []
-            # mapped_seeds = copy(seeds)
-            while length(seeds) > 0
-                seed = popfirst!(seeds)
-                println("\nmapping $(seed)")
-                did_seed_map = false
-                for line in split(map, "\n")
-                    if line == ""
-                        continue
-                    elseif occursin(':', line)
-                        name = split(line)[1]
-                        println(name)
-                    else
-                        # try to map each seed to this category
-                        dest_start, source_start, len = parse.(Int, split(line))
-                        source = source_start:source_start+len-1
-                        dest = dest_start:dest_start+len-1
-                        println("$source, $dest")
-                        matching = intersect(seed, source)
-                        if length(matching) == 0
-                            continue
-                        end
-                        println("matching: $matching")
-                        dest_range = matching .+ (dest_start - source_start)
-                        println("dest: $dest_range")
-                        # println("match to $(collect(dest_range))")
-                        push!(mapped_seeds, dest_range)
-                        if length(dest_range) != length(seed)
-                            # residual = setdiff(seed, source)
-                            # println("residual: $residual")
-                            # println("dogfeeding $(collect(residual))")
-                            if seed[begin] < source_start
-                                residual = seed[begin]:source_start-1
-                                println("residual: $residual")
-                                push!(seeds, residual)
-                            end
-                            if seed[end] > source_start + len
-                                residual = source_start+len:seed[end]
-                                println("residual: $residual")
-                                push!(seeds, residual)
-                            end
-                        end
-                        did_seed_map = true
-                        break
-                    end
-                end
-                if did_seed_map == false
-                    # println("couldn't match $seed")
-                    push!(mapped_seeds, seed)
+        seeds = parse.(Int, split(split(readline(io), ": ")[2]))
+        maps = OrderedDict()
+        for map in eachsplit(read(io, String), "\n\n")
+            name = ""
+            for line in eachsplit(map, "\n")
+                if "" == line
+                    continue
+                elseif occursin(':', line)
+                    name = split(line)[1]
+                    maps[name] = []
+                else
+                    dest_start, source_start, len = parse.(Int, split(line))
+                    push!(maps[name], (dest_start:dest_start+len-1, source_start:source_start+len-1))
                 end
             end
-            seeds = mapped_seeds
-            # println(seeds)
         end
-        println("mapped $seeds")
-        println(minimum([x[1] for x in seeds]))
-        # println( minimum(seeds))
+        return seeds, maps
     end
 end
 
+function part1(seeds, maps)
+    for map in values(maps)
+        mapped_seeds = copy(seeds)
+        for (i, seed) in enumerate(seeds)
+            for (dest, source) in map
+                if seed in source
+                    mapped_seeds[i] = seed + dest[begin] - source[begin]
+                    break
+                end
+            end
+        end
+        seeds = mapped_seeds
+    end
+    return minimum(seeds)
+end
 
-l = read_puzzle("input")
+function part2(pre_seeds, maps)
+    seeds = broadcast(:, pre_seeds[1:2:end], pre_seeds[1:2:end] .+ pre_seeds[2:2:end] .- 1)
+    for (map_name, map) in maps
+        mapped_seeds = []
+        while length(seeds) > 0
+            seed = popfirst!(seeds)
+            was_seed_mapped = false
+            for (dest, source) in map
+                matching = intersect(seed, source)
+                if 0 == length(matching)
+                    continue
+                end
+                mapped_seed = matching .+ (dest[begin] - source[begin])
+                push!(mapped_seeds, mapped_seed)
+                if length(mapped_seed) != length(seed)
+                    if seed[begin] < source[begin]
+                        residual = seed[begin]:source[begin]-1
+                        push!(seeds, residual)
+                    end
+                    if seed[end] > source[end]
+                        residual = source[end]+1:seed[end]
+                        push!(seeds, residual)
+                    end
+                end
+                was_seed_mapped = true
+                break
+            end
+            if false == was_seed_mapped
+                push!(mapped_seeds, seed)
+            end
+        end
+        seeds = mapped_seeds
+    end
+    return minimum([x[begin] for x in seeds])
+end
+
+
+seeds, maps = read_puzzle("input")
+println(part1(seeds, maps))
+println(part2(seeds, maps))
