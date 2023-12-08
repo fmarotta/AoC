b14ccb0185b7f87156a74ec701e1395a3158c15f
Federico Marotta <fmarotta@protonmail.com>
2023-12-08 09:16:58 +0100

    cosmetic improvements

--- 2023/day8/run.jl
+++ 2023/day8/run.jl
@@ -1,19 +1,16 @@
 function read_puzzle(file)
-    open(file, "r") do io
-        directions = readline(io)
-        directions = split(directions, "")
-        directions = replace(directions, "L" => "1", "R" => "2")
-        directions = parse.(Int, directions)
-        network = Dict()
-        for line in readlines(io)
-            if line == ""
-                continue
-            end
-            m = match(r"(.*) = \((.*), (.*)\)", line)
-            network[m[1]] = (m[2], m[3])
+    directions, connections = split(read(file, String), "\n\n")
+    directions = split(directions, "")
+    directions = replace(directions, "L" => 1, "R" => 2)
+    network = Dict()
+    for line in split(connections, "\n")
+        if line == ""
+            continue
         end
-        return directions, network
+        m = match(r"(.*) = \((.*), (.*)\)", line)
+        network[m[1]] = (m[2], m[3])
     end
+    return directions, network
 end
 
 function count_moves_part1(directions, network, start = "AAA", targets = ["ZZZ"])
@@ -29,8 +26,8 @@ function count_moves_part1(directions, network, start = "AAA", targets = ["ZZZ"]
 end
 
 function count_moves_part2(directions, network)
-    starts = filter(x -> x[3] == 'A', collect(keys(network)))
-    targets = filter(x -> x[3] == 'Z', collect(keys(network)))
+    starts = filter(endswith("A"), keys(network))
+    targets = filter(endswith("Z"), keys(network))
     periods = [count_moves_part1(directions, network, c, targets) for c in starts]
     return lcm(periods)
 end
